f54ff817037b64ebe904a3f0d074c4aa9b588f73
Alex Barbosa <abarbosasilva7@gmail.com>
2020-07-02 00:16:02 -0300

    Testando a primeira classe de dominio - Estado

--- src/main/java/com/alugueaki/project/resources/EstadoResource.java
+++ src/main/java/com/alugueaki/project/resources/EstadoResource.java
@@ -1,16 +1,29 @@
 package com.alugueaki.project.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alugueaki.project.domain.Estado;
 
 @RestController
 @RequestMapping(value="/estados")
 public class EstadoResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando !!!";
+	public List<Estado> listar() {
+		
+		Estado uf = new Estado(1, "Pernambuco");
+		Estado uf1 = new Estado(1, "Bahia");
+		
+		List<Estado> list = new ArrayList<>();
+		list.add(uf);
+		list.add(uf1);
+		
+		return list;
 	}
 
 }
