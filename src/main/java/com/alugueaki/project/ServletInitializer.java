767a41fe5977ede780c9c7beedf6af7af0ab9b64
Alex Barbosa <abarbosasilva7@gmail.com>
2020-07-01 22:58:30 -0300

    Projeto Criado

---
+++ src/main/java/com/alugueaki/project/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.alugueaki.project;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AlugueakiApplication.class);
+	}
+
+}
---
+++ src/test/java/com/alugueaki/project/AlugueakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alugueaki.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugueakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
